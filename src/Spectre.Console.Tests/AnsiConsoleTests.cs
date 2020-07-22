730f812fc6b2da8af3c6d80c628e4c63f0d57c8e
Patrik Svensson <patrik@patriksvensson.se>
2020-07-22 11:24:44 +0200

    Add WriteLine extensions for IAnsiConsole

--- src/Spectre.Console.Tests/AnsiConsoleTests.cs
+++ src/Spectre.Console.Tests/AnsiConsoleTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using Shouldly;
 using Xunit;
 
@@ -67,6 +69,179 @@ namespace Spectre.Console.Tests
 
             // Then
             fixture.Output.ShouldBe("\u001b[90;47mHello\u001b[0m");
+        }
+
+        public sealed class WriteLine
+        {
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Int32_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, 32);
+
+                // Then
+                fixture.Output.ShouldBe("32" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_UInt32_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, 32U);
+
+                // Then
+                fixture.Output.ShouldBe("32" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Int64_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, 32L);
+
+                // Then
+                fixture.Output.ShouldBe("32" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_UInt64_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, 32UL);
+
+                // Then
+                fixture.Output.ShouldBe("32" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Single_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, 32.432F);
+
+                // Then
+                fixture.Output.ShouldBe("32.432" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Double_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, (double)32.432);
+
+                // Then
+                fixture.Output.ShouldBe("32.432" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Decimal_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, 32.432M);
+
+                // Then
+                fixture.Output.ShouldBe("32.432" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Boolean_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, true);
+
+                // Then
+                fixture.Output.ShouldBe("True" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Char_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(CultureInfo.InvariantCulture, 'P');
+
+                // Then
+                fixture.Output.ShouldBe("P" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Char_Array_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(
+                    CultureInfo.InvariantCulture,
+                    new[] { 'P', 'a', 't', 'r', 'i', 'k' });
+
+                // Then
+                fixture.Output.ShouldBe("Patrik" + Environment.NewLine);
+            }
+
+            [Theory]
+            [InlineData(AnsiSupport.Yes)]
+            [InlineData(AnsiSupport.No)]
+            public void Should_Write_Formatted_String_With_Format_Provider(AnsiSupport ansi)
+            {
+                // Given
+                var fixture = new AnsiConsoleFixture(ColorSystem.Standard, ansi);
+
+                // When
+                fixture.Console.WriteLine(
+                    CultureInfo.InvariantCulture,
+                    "Hello {0}! {1}",
+                    "World", 32);
+
+                // Then
+                fixture.Output.ShouldBe("Hello World! 32" + Environment.NewLine);
+            }
         }
     }
 }
