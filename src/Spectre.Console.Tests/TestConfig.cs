bde61cc6ff053ab1cd1b5c6784e1143955de631f
Simon Cropp <simon.cropp@gmail.com>
2020-11-21 09:27:35 +1100

    Verify: Use custom test directory

---
+++ src/Spectre.Console.Tests/TestConfig.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Spectre.Console.Tests
+{
+    public static class TestConfig
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            VerifyTests.VerifierSettings.DeriveTestDirectory((_, directory) =>
+            {
+                var expectations = Path.Combine(directory, "Expectations");
+                if (!Directory.Exists(expectations))
+                {
+                    Directory.CreateDirectory(expectations);
+                }
+
+                return expectations;
+            });
+        }
+    }
+}
