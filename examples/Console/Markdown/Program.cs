5cb12ce2f04b9b94d53e2daf52de91ad7233af31
Matthew Constable <bonsty@gmail.com>
2021-01-02 14:10:24 +0000

    Create Markdown project for testing with proper terminal.

---
+++ examples/Console/Markdown/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Spectre.Console;
+
+namespace MarkdownExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var resource = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("Markdown.Sample.md");
+            var markdown = new StreamReader(resource ?? throw new InvalidOperationException()).ReadToEnd();
+
+            var markdownWidget = new Markdown(markdown);
+
+            AnsiConsole.Render(markdownWidget);
+        }
+    }
+}
