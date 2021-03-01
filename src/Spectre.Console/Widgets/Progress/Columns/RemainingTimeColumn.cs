064d0072a17c39107ef77fe1884b9fa139ba6d9d
MarkPflug <mark.pflug@live.com>
2021-03-01 12:15:35 -0800

    Progress time remaining column renders elapsed when complete.

--- src/Spectre.Console/Widgets/Progress/Columns/RemainingTimeColumn.cs
+++ src/Spectre.Console/Widgets/Progress/Columns/RemainingTimeColumn.cs
@@ -8,6 +8,18 @@ namespace Spectre.Console
     /// </summary>
     public sealed class RemainingTimeColumn : ProgressColumn
     {
+        public RemainingTimeColumn()
+            : this(false)
+        {
+        }
+
+        public RemainingTimeColumn(bool elapsedWhenFinished)
+        {
+            _elapsedWhenFinished = elapsedWhenFinished;
+        }
+
+        private readonly bool _elapsedWhenFinished;
+
         /// <inheritdoc/>
         protected internal override bool NoWrap => true;
 
@@ -16,21 +28,33 @@ namespace Spectre.Console
         /// </summary>
         public Style Style { get; set; } = new Style(foreground: Color.Blue);
 
+        /// <summary>
+        /// Gets or sets the style of the remaining time text.
+        /// </summary>
+        public Style FinishedStyle { get; set; } = new Style(foreground: Color.Green);
+
         /// <inheritdoc/>
         public override IRenderable Render(RenderContext context, ProgressTask task, TimeSpan deltaTime)
         {
-            var remaining = task.RemainingTime;
-            if (remaining == null)
+            if (_elapsedWhenFinished && task.IsFinished)
             {
-                return new Markup("--:--:--");
+                return new Text($"{task.ElapsedTime:hh\\:mm\\:ss}", FinishedStyle ?? Style.Plain);
             }
-
-            if (remaining.Value.TotalHours > 99)
+            else
             {
-                return new Markup("**:**:**");
-            }
+                var remaining = task.RemainingTime;
+                if (remaining == null)
+                {
+                    return new Markup("--:--:--");
+                }
 
-            return new Text($"{remaining.Value:hh\\:mm\\:ss}", Style ?? Style.Plain);
+                if (remaining.Value.TotalHours > 99)
+                {
+                    return new Markup("**:**:**");
+                }
+
+                return new Text($"{remaining.Value:hh\\:mm\\:ss}", Style ?? Style.Plain);
+            }
         }
 
         /// <inheritdoc/>
