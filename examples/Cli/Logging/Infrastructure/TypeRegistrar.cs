1cd335e7856435555445d4ea4322708f1d8bba2c
Phil Scott <phil.scott@gmail.com>
2021-03-04 02:39:53 -0500

    Serilog example for logging

---
+++ examples/Cli/Logging/Infrastructure/TypeRegistrar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+namespace Logging
+{
+    public sealed class TypeRegistrar : ITypeRegistrar
+    {
+        private readonly IServiceCollection _builder;
+
+        public TypeRegistrar(IServiceCollection builder)
+        {
+            _builder = builder;
+        }
+
+        public ITypeResolver Build()
+        {
+            return new TypeResolver(_builder.BuildServiceProvider());
+        }
+
+        public void Register(Type service, Type implementation)
+        {
+            _builder.AddSingleton(service, implementation);
+        }
+
+        public void RegisterInstance(Type service, object implementation)
+        {
+            _builder.AddSingleton(service, implementation);
+        }
+
+        public void RegisterLazy(Type service, Func<object> func)
+        {
+            if (func is null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            _builder.AddSingleton(service, _ => func());
+        }
+    }
+}
