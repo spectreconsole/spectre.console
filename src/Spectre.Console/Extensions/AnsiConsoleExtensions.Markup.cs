8af9de02541d03449326a3e251a78bd34a3f44ac
Cédric Luthi <cedric.luthi@gmail.com>
2021-09-29 10:02:57 +0200

    Introduce MarkupInterpolated and MarkupLineInterpolated extensions
    These new methods enable easily writing markup with a nice and intuitive syntax without having to worry about escaping the markup.
    
    ```csharp
    string input = args[0];
    string output = Process(input);
    AnsiConsole.MarkupLineInterpolated($"[blue]{input}[/] -> [green]{output}[/]");
    
    ```
    
    The `Interpolated` suffix was inspired by the Entity Framework Core [FromSqlInterpolated][1] method.
    
    [1]: https://docs.microsoft.com/en-us/ef/core/querying/raw-sql#passing-parameters

--- src/Spectre.Console/Extensions/AnsiConsoleExtensions.Markup.cs
+++ src/Spectre.Console/Extensions/AnsiConsoleExtensions.Markup.cs
@@ -16,27 +16,66 @@ public static partial class AnsiConsoleExtensions
         Markup(console, CultureInfo.CurrentCulture, format, args);
     }
 
-    /// <summary>
-    /// Writes the specified markup to the console.
-    /// </summary>
-    /// <param name="console">The console to write to.</param>
-    /// <param name="provider">An object that supplies culture-specific formatting information.</param>
-    /// <param name="format">A composite format string.</param>
-    /// <param name="args">An array of objects to write.</param>
-    public static void Markup(this IAnsiConsole console, IFormatProvider provider, string format, params object[] args)
-    {
-        Markup(console, string.Format(provider, format, args));
-    }
+        /// <summary>
+        /// Writes the specified markup to the console.
+        /// <para/>
+        /// All interpolation holes which contain a string are automatically escaped so you must not call <see cref="StringExtensions.EscapeMarkup"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// string input = args[0];
+        /// string output = Process(input);
+        /// console.MarkupInterpolated($"[blue]{input}[/] -> [green]{output}[/]");
+        /// </code>
+        /// </example>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="value">The interpolated string value to write.</param>
+        public static void MarkupInterpolated(this IAnsiConsole console, FormattableString value)
+        {
+            MarkupInterpolated(console, CultureInfo.CurrentCulture, value);
+        }
 
-    /// <summary>
-    /// Writes the specified markup to the console.
-    /// </summary>
-    /// <param name="console">The console to write to.</param>
-    /// <param name="value">The value to write.</param>
-    public static void Markup(this IAnsiConsole console, string value)
-    {
-        console.Write(MarkupParser.Parse(value));
-    }
+        /// <summary>
+        /// Writes the specified markup to the console.
+        /// </summary>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="format">A composite format string.</param>
+        /// <param name="args">An array of objects to write.</param>
+        public static void Markup(this IAnsiConsole console, IFormatProvider provider, string format, params object[] args)
+        {
+            Markup(console, string.Format(provider, format, args));
+        }
+
+        /// <summary>
+        /// Writes the specified markup to the console.
+        /// <para/>
+        /// All interpolation holes which contain a string are automatically escaped so you must not call <see cref="StringExtensions.EscapeMarkup"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// string input = args[0];
+        /// string output = Process(input);
+        /// console.MarkupInterpolated(CultureInfo.InvariantCulture, $"[blue]{input}[/] -> [green]{output}[/]");
+        /// </code>
+        /// </example>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="value">The interpolated string value to write.</param>
+        public static void MarkupInterpolated(this IAnsiConsole console, IFormatProvider provider, FormattableString value)
+        {
+            MarkupInterpolated(console, provider, value, appendNewLine: false);
+        }
+
+        /// <summary>
+        /// Writes the specified markup to the console.
+        /// </summary>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="value">The value to write.</param>
+        public static void Markup(this IAnsiConsole console, string value)
+        {
+            console.Write(MarkupParser.Parse(value));
+        }
 
     /// <summary>
     /// Writes the specified markup, followed by the current line terminator, to the console.
@@ -49,25 +88,72 @@ public static partial class AnsiConsoleExtensions
         MarkupLine(console, CultureInfo.CurrentCulture, format, args);
     }
 
-    /// <summary>
-    /// Writes the specified markup, followed by the current line terminator, to the console.
-    /// </summary>
-    /// <param name="console">The console to write to.</param>
-    /// <param name="value">The value to write.</param>
-    public static void MarkupLine(this IAnsiConsole console, string value)
-    {
-        Markup(console, value + Environment.NewLine);
-    }
+        /// <summary>
+        /// Writes the specified markup, followed by the current line terminator, to the console.
+        /// <para/>
+        /// All interpolation holes which contain a string are automatically escaped so you must not call <see cref="StringExtensions.EscapeMarkup"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// string input = args[0];
+        /// string output = Process(input);
+        /// console.MarkupLineInterpolated($"[blue]{input}[/] -> [green]{output}[/]");
+        /// </code>
+        /// </example>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="value">The interpolated string value to write.</param>
+        public static void MarkupLineInterpolated(this IAnsiConsole console, FormattableString value)
+        {
+            MarkupLineInterpolated(console, CultureInfo.CurrentCulture, value);
+        }
 
-    /// <summary>
-    /// Writes the specified markup, followed by the current line terminator, to the console.
-    /// </summary>
-    /// <param name="console">The console to write to.</param>
-    /// <param name="provider">An object that supplies culture-specific formatting information.</param>
-    /// <param name="format">A composite format string.</param>
-    /// <param name="args">An array of objects to write.</param>
-    public static void MarkupLine(this IAnsiConsole console, IFormatProvider provider, string format, params object[] args)
-    {
-        Markup(console, provider, format + Environment.NewLine, args);
+        /// <summary>
+        /// Writes the specified markup, followed by the current line terminator, to the console.
+        /// </summary>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="value">The value to write.</param>
+        public static void MarkupLine(this IAnsiConsole console, string value)
+        {
+            Markup(console, value + Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Writes the specified markup, followed by the current line terminator, to the console.
+        /// </summary>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="format">A composite format string.</param>
+        /// <param name="args">An array of objects to write.</param>
+        public static void MarkupLine(this IAnsiConsole console, IFormatProvider provider, string format, params object[] args)
+        {
+            Markup(console, provider, format + Environment.NewLine, args);
+        }
+
+        /// <summary>
+        /// Writes the specified markup, followed by the current line terminator, to the console.
+        /// <para/>
+        /// All interpolation holes which contain a string are automatically escaped so you must not call <see cref="StringExtensions.EscapeMarkup"/>.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// string input = args[0];
+        /// string output = Process(input);
+        /// console.MarkupLineInterpolated(CultureInfo.InvariantCulture, $"[blue]{input}[/] -> [green]{output}[/]");
+        /// </code>
+        /// </example>
+        /// <param name="console">The console to write to.</param>
+        /// <param name="provider">An object that supplies culture-specific formatting information.</param>
+        /// <param name="value">The interpolated string value to write.</param>
+        public static void MarkupLineInterpolated(this IAnsiConsole console, IFormatProvider provider, FormattableString value)
+        {
+            MarkupInterpolated(console, provider, value, appendNewLine: true);
+        }
+
+        private static void MarkupInterpolated(this IAnsiConsole console, IFormatProvider provider, FormattableString value, bool appendNewLine)
+        {
+            object?[] args = value.GetArguments().Select(arg => arg is string s ? s.EscapeMarkup() : arg).ToArray();
+            var format = appendNewLine ? value.Format + Environment.NewLine : value.Format;
+            Markup(console, string.Format(provider, format, args));
+        }
     }
 }
