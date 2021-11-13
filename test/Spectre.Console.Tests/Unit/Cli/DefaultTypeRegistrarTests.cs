8765dc90025cb9cab6b50bd26bd851356c6798b7
Nils Andresen <nils@nils-andresen.de>
2021-11-13 21:33:58 +0100

    (#555) added TypeRegistrarBaseTests
    as a very simple test harness to test implementations of
    ITypeRegistrar / ITypeResolver

---
+++ test/Spectre.Console.Tests/Unit/Cli/DefaultTypeRegistrarTests.cs
@@ -0,0 +1,16 @@
+using Spectre.Console.Cli;
+using Spectre.Console.Testing;
+using Xunit;
+
+namespace Spectre.Console.Tests.Unit.Cli
+{
+    public sealed class DefaultTypeRegistrarTests
+    {
+        [Fact]
+        public void Should_Pass_Base_Registrar_Tests()
+        {
+            var harness = new TypeRegistrarBaseTests(() => new DefaultTypeRegistrar());
+            harness.RunAllTests();
+        }
+    }
+}
