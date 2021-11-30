81b868eb461a42d87f95f2a606c006831f4f5d70
Phil Scott <phil.scott@gmail.com>
2021-11-30 10:42:22 -0500

    Updating asciinema

--- docs/Program.cs
+++ docs/Program.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Docs.Shortcodes;
 using Statiq.App;
 using Statiq.Common;
+using Statiq.Core;
 using Statiq.Web;
 
 namespace Docs
@@ -16,12 +17,20 @@ namespace Docs
                 .AddSetting(Constants.EditLink, ConfigureEditLink())
                 .ConfigureSite("spectreconsole", "spectre.console", "main")
                 .ConfigureDeployment(deployBranch: "docs")
+                
                 .AddShortcode("Children", typeof(ChildrenShortcode))
                 .AddShortcode("ColorTable", typeof(ColorTableShortcode))
                 .AddShortcode("EmojiTable", typeof(EmojiTableShortcode))
                 .AddShortcode("Alert", typeof(AlertShortcode))
                 .AddShortcode("AsciiCast", typeof(AsciiCastShortcode))
                 .AddPipelines()
+                .BuildPipeline(
+			        "Bootstrap",
+			            builder => builder
+				            .WithInputReadFiles("../node_modules/asciinema-player/dist/bundle/asciinema-player.js")
+				            .WithProcessModules(new SetDestination(Config.FromDocument(doc => new NormalizedPath($"./assets/{doc.Source.FileName}")), true))
+				    .WithOutputWriteFiles()
+                )
                 .AddProcess(ProcessTiming.Initialization, _ => new ProcessLauncher("npm", "install --audit false --fund false --progress false")
                 {
                     LogErrors = false
