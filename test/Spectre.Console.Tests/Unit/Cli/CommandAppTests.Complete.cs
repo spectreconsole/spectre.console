a88f2722aabb810956b2c6d6eaf43452d11003ac
Jos Koelewijn <joskw@live.com>
2022-02-06 21:25:21 +0100

    Add some more tests

--- test/Spectre.Console.Tests/Unit/Cli/CommandAppTests.Complete.cs
+++ test/Spectre.Console.Tests/Unit/Cli/CommandAppTests.Complete.cs
@@ -20,7 +20,7 @@ public sealed partial class CommandAppTests
             });
             var commandToRun = Constants.CompleteCommand
                    .ToList()
-                   .Append("myapp li");
+                   .Append("\"myapp li\"");
 
             // When
             var result = fixture.Run(commandToRun.ToArray());
@@ -28,5 +28,84 @@ public sealed partial class CommandAppTests
             // Then
             return Verifier.Verify(result.Output);
         }
+
+        [Fact]
+        [Expectation("Test_2")]
+        public Task Should_Return_Correct_Completions_For_Case_2()
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.Configure(config =>
+            {
+                config.SetApplicationName("myapp");
+                config.PropagateExceptions();
+                config.AddCommand<LionCommand>("lion");
+                config.AddCommand<CatCommand>("cat");
+            });
+            var commandToRun = Constants.CompleteCommand
+                   .ToList()
+                   .Append("\"myapp \"");
+
+            // When
+            var result = fixture.Run(commandToRun.ToArray());
+
+            // Then
+            return Verifier.Verify(result.Output);
+        }
+
+        [Fact]
+        [Expectation("Test_3")]
+        public Task Should_Return_Correct_Completions_For_Case_3()
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.Configure(config =>
+            {
+                config.SetApplicationName("myapp");
+                config.PropagateExceptions();
+                config.AddBranch("animal", animal =>
+                {
+                    animal.AddCommand<LionCommand>("lion");
+                    animal.AddCommand<CatCommand>("cat");
+                });
+            });
+            var commandToRun = Constants.CompleteCommand
+                   .ToList()
+                   .Append("\"myapp \"");
+
+            // When
+            var result = fixture.Run(commandToRun.ToArray());
+
+            // Then
+            return Verifier.Verify(result.Output);
+        }
+
+        [Fact]
+        [Expectation("Test_4")]
+        public Task Should_Return_Correct_Completions_For_Case_4()
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.Configure(config =>
+            {
+                config.SetApplicationName("myapp");
+                config.PropagateExceptions();
+                config.AddBranch("animal", animal =>
+                {
+                    animal.AddCommand<LionCommand>("lion");
+                    animal.AddCommand<CatCommand>("cat");
+                });
+            });
+            var commandToRun = Constants.CompleteCommand
+                   .ToList()
+                   .Append("\"myapp animal \"");
+
+            // When
+            var result = fixture.Run(commandToRun.ToArray());
+
+            // Then
+            return Verifier.Verify(result.Output);
+        }
+
     }
 }
