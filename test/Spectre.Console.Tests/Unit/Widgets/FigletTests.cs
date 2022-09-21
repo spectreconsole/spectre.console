999c59909b524eae3f17b1d2e5103f28d9b87a28
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-09-21 19:03:11 +0200

    Fix @ being used in figlet font

--- test/Spectre.Console.Tests/Unit/Widgets/FigletTests.cs
+++ test/Spectre.Console.Tests/Unit/Widgets/FigletTests.cs
@@ -4,20 +4,23 @@ namespace Spectre.Console.Tests.Unit;
 [ExpectationPath("Widgets/Figlet")]
 public sealed class FigletTests
 {
-    [Fact]
+    [Theory]
+    [InlineData("starwars.flf")]
+    [InlineData("poison.flf")]
     [Expectation("Load_Stream")]
-    public async Task Should_Load_Font_From_Stream()
+    public async Task Should_Load_Font_From_Stream(string fontfile)
     {
         // Given
         var console = new TestConsole().Width(180);
-        var font = FigletFont.Load(EmbeddedResourceReader.LoadResourceStream("Spectre.Console.Tests/Data/starwars.flf"));
+        var font = FigletFont.Load(EmbeddedResourceReader.LoadResourceStream($"Spectre.Console.Tests/Data/{fontfile}"));
         var text = new FigletText(font, "Patrik was here");
 
         // When
         console.Write(text);
 
         // Then
-        await Verifier.Verify(console.Output);
+        await Verifier.Verify(console.Output)
+            .UseParameters(fontfile);
     }
 
     [Fact]
