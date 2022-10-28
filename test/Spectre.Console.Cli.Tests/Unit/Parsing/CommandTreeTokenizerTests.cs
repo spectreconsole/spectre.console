5d2b76f6b95ce26a0718182fd3eeecc7fa611774
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2022-10-28 17:58:38 +0100

    More extensive unit test coverage for POSIX compliant handling of quotes, hyphens and spaces by the CommandTreeTokenizer

--- test/Spectre.Console.Cli.Tests/Unit/Parsing/CommandTreeTokenizerTests.cs
+++ test/Spectre.Console.Cli.Tests/Unit/Parsing/CommandTreeTokenizerTests.cs
@@ -3,16 +3,10 @@ namespace Spectre.Console.Tests.Unit.Cli.Parsing;
 public class CommandTreeTokenizerTests
 {
     [Theory]
-    [InlineData("\"\"")]
-    [InlineData("\" \"")]
-
-    // Special character handling
-    [InlineData("-R")]
-    [InlineData("-Rufus")]
-    [InlineData("--R")]
-    [InlineData("--Rufus")]
 
     // Double-quote handling
+    [InlineData("\"")]
+    [InlineData("\"\"")]
     [InlineData("\"Rufus\"")]
     [InlineData("\" Rufus\"")]
     [InlineData("\"-R\"")]
@@ -20,12 +14,14 @@ public class CommandTreeTokenizerTests
     [InlineData("\" -Rufus\"")]
 
     // Single-quote handling
+    [InlineData("'")]
+    [InlineData("''")]
     [InlineData("'Rufus'")]
     [InlineData("' Rufus'")]
     [InlineData("'-R'")]
     [InlineData("'-Rufus'")]
     [InlineData("' -Rufus'")]
-    public void Quote_Tests(string actualAndExpected)
+    public void Should_Preserve_Quotes(string actualAndExpected)
     {
         // Given
 
@@ -35,5 +31,69 @@ public class CommandTreeTokenizerTests
         // Then
         result.Tokens.Count.ShouldBe(1);
         result.Tokens[0].Value.ShouldBe(actualAndExpected);
+    }
+
+    [Theory]
+    [InlineData("Rufus-")]
+    [InlineData("Rufus--")]
+    [InlineData("R-u-f-u-s")]
+    public void Should_Preserve_Hyphen_Delimiters(string actualAndExpected)
+    {
+        // Given
+
+        // When
+        var result = CommandTreeTokenizer.Tokenize(new string[] { actualAndExpected });
+
+        // Then
+        result.Tokens.Count.ShouldBe(1);
+        result.Tokens[0].Value.ShouldBe(actualAndExpected);
+    }
+
+    [Theory]
+    [InlineData(" Rufus")]
+    [InlineData("Rufus ")]
+    [InlineData(" Rufus ")]
+    public void Should_Preserve_Spaces(string actualAndExpected)
+    {
+        // Given
+
+        // When
+        var result = CommandTreeTokenizer.Tokenize(new string[] { actualAndExpected });
+
+        // Then
+        result.Tokens.Count.ShouldBe(1);
+        result.Tokens[0].Value.ShouldBe(actualAndExpected);
+    }
+
+    [Theory]
+    [InlineData(" \" -Rufus -- ")]
+    [InlineData("Name=\" -Rufus --' ")]
+    public void Should_Preserve_Quotes_Hyphen_Delimiters_Spaces(string actualAndExpected)
+    {
+        // Given
+
+        // When
+        var result = CommandTreeTokenizer.Tokenize(new string[] { actualAndExpected });
+
+        // Then
+        result.Tokens.Count.ShouldBe(1);
+        result.Tokens[0].Value.ShouldBe(actualAndExpected);
+    }
+
+    [Theory]
+    [InlineData("--Name=\" -Rufus --' ", "Name", "\" -Rufus --' ")]
+    [InlineData("-N=\" -Rufus --' ", "N", "\" -Rufus --' ")]
+    [InlineData("-N:\" -Rufus --' ", "N", "\" -Rufus --' ")]
+    public void Should_Preserve_Quotes_Hyphen_Delimiters_Spaces2(string actual, string expectedToken1Value, string expectedToken2Value)
+    {
+        // Given
+
+        // When
+        var result = CommandTreeTokenizer.Tokenize(new string[] { actual });
+
+        // Then
+        result.Tokens.Count.ShouldBe(2);
+        result.Tokens[0].Value.ShouldBe(expectedToken1Value);
+        result.Tokens[1].Value.ShouldBe(expectedToken2Value);
     }
 }
