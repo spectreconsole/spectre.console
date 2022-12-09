d2bf34d7a2ff1f13b3c5461beadcb57ca644c4dd
nsnail <taokeu@gmail.com>
2022-12-09 17:29:46 +0800

    Update LocalizationAttribute.cs
    additional xml documents

--- src/Spectre.Console.Cli/Annotations/LocalizationAttribute.cs
+++ src/Spectre.Console.Cli/Annotations/LocalizationAttribute.cs
@@ -1,13 +1,22 @@
-﻿namespace Spectre.Console.Cli;
+namespace Spectre.Console.Cli;
 
+/// <summary>
+/// Indicates that a "Description" feature should display its localization description.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
 public class LocalizationAttribute : Attribute
 {
     /// <summary>
-    ///     A strongly-typed resource class, for looking up localized strings, etc.
+    ///   Gets or Sets a strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
     public Type ResourceClass { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LocalizationAttribute"/> class.
+    /// </summary>
+    /// <param name="resourceClass">
+    ///      The type of the resource manager.
+    /// </param>
     public LocalizationAttribute(Type resourceClass)
     {
         ResourceClass = resourceClass;
