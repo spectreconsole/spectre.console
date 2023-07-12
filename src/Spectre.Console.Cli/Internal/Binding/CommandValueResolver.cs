spectreconsole/spectre.console

commit d3f4f5f208ee76a22ff39c13df4d150712685f5f

Add support for converting command parameters into FileInfo and DirectoryInfo (#1145)
Add support for converting command parameters that doesn't have a built-in TypeConverter but has a constructor that takes a string. For CLI apps, FileInfo and DirectoryInfo will likely be the most useful ones, but there may be others.

src/Spectre.Console.Cli/Internal/Binding/CommandValueResolver.cs
@@ -78,18 +78,26 @@ internal static class CommandValueResolver
                     }
                     else
                     {
-                        var converter = GetConverter(lookup, binder, resolver, mapped.Parameter);
+                        var (converter, stringConstructor) = GetConverter(lookup, binder, resolver, mapped.Parameter);
                         if (converter == null)
                         {
                             throw CommandRuntimeException.NoConverterFound(mapped.Parameter);
                         }
 
                         object? value;
+                        var mappedValue = mapped.Value ?? string.Empty;
                         try
                         {
-                            value = converter.ConvertFromInvariantString(mapped.Value ?? string.Empty);
+                            try
+                            {
+                                value = converter.ConvertFromInvariantString(mappedValue);
+                            }
+                            catch (NotSupportedException) when (stringConstructor != null)
+                            {
+                                value = stringConstructor.Invoke(new object[] { mappedValue });
+                            }
                         }
-                        catch (Exception exception)
+                        catch (Exception exception) when (exception is not CommandRuntimeException)
                         {
                             throw CommandRuntimeException.ConversionFailed(mapped, converter, exception);
                         }
@@ -122,7 +130,7 @@ internal static class CommandValueResolver
     {
         if (result != null && result.GetType() != parameter.ParameterType)
         {
-            var converter = GetConverter(lookup, binder, resolver, parameter);
+            var (converter, _) = GetConverter(lookup, binder, resolver, parameter);
             if (converter != null)
             {
                 result = converter.ConvertFrom(result);
@@ -133,8 +141,14 @@ internal static class CommandValueResolver
     }
 
     [SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "It's OK")]
-    private static TypeConverter? GetConverter(CommandValueLookup lookup, CommandValueBinder binder, ITypeResolver resolver, CommandParameter parameter)
+    private static (TypeConverter? Converter, ConstructorInfo? StringConstructor) GetConverter(CommandValueLookup lookup, CommandValueBinder binder, ITypeResolver resolver, CommandParameter parameter)
     {
+        static ConstructorInfo? GetStringConstructor(Type type)
+        {
+            var constructor = type.GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, new[] { typeof(string) }, null);
+            return constructor?.GetParameters()[0].ParameterType == typeof(string) ? constructor : null;
+        }
+
         if (parameter.Converter == null)
         {
             if (parameter.ParameterType.IsArray)
@@ -146,12 +160,12 @@ internal static class CommandValueResolver
                     throw new InvalidOperationException("Could not get element type");
                 }
 
-                return TypeDescriptor.GetConverter(elementType);
+                return (TypeDescriptor.GetConverter(elementType), GetStringConstructor(elementType));
             }
 
             if (parameter.IsFlagValue())
             {
-                // Is the optional value instanciated?
+                // Is the optional value instantiated?
                 var value = lookup.GetValue(parameter) as IFlagValue;
                 if (value == null)
                 {
@@ -161,18 +175,18 @@ internal static class CommandValueResolver
                     value = lookup.GetValue(parameter) as IFlagValue;
                     if (value == null)
                     {
-                        throw new InvalidOperationException("Could not intialize optional value.");
+                        throw new InvalidOperationException("Could not initialize optional value.");
                     }
                 }
 
                 // Return a converter for the flag element type.
-                return TypeDescriptor.GetConverter(value.Type);
+                return (TypeDescriptor.GetConverter(value.Type), GetStringConstructor(value.Type));
             }
 
-            return TypeDescriptor.GetConverter(parameter.ParameterType);
+            return (TypeDescriptor.GetConverter(parameter.ParameterType), GetStringConstructor(parameter.ParameterType));
         }
 
         var type = Type.GetType(parameter.Converter.ConverterTypeName);
-        return resolver.Resolve(type) as TypeConverter;
+        return (resolver.Resolve(type) as TypeConverter, null);
     }
 }

test/Spectre.Console.Cli.Tests/Data/Settings/HorseSettings.cs
@@ -1,7 +1,15 @@
+using System.IO;
+
 namespace Spectre.Console.Tests.Data;
 
 public class HorseSettings : MammalSettings
 {
     [CommandOption("-d|--day")]
     public DayOfWeek Day { get; set; }
+
+    [CommandOption("--file")]
+    public FileInfo File { get; set; }
+
+    [CommandOption("--directory")]
+    public DirectoryInfo Directory { get; set; }
 }

test/Spectre.Console.Cli.Tests/Unit/CommandAppTests.TypeConverters.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace Spectre.Console.Tests.Unit.Cli;
 
 public sealed partial class CommandAppTests
@@ -75,6 +77,28 @@ public sealed partial class CommandAppTests
             result.Output.ShouldContain(nameof(DayOfWeek.Thursday));
             result.Output.ShouldContain(nameof(DayOfWeek.Friday));
             result.Output.ShouldContain(nameof(DayOfWeek.Saturday));
+        }
+
+        [Fact]
+        public void Should_Convert_FileInfo_And_DirectoryInfo()
+        {
+            // Given
+            var app = new CommandAppTester();
+            app.Configure(config =>
+            {
+                config.AddCommand<HorseCommand>("horse");
+            });
+
+            // When
+            var result = app.Run(new[] { "horse", "--file", "ntp.conf", "--directory", "etc" });
+
+            // Then
+            result.ExitCode.ShouldBe(0);
+            result.Settings.ShouldBeOfType<HorseSettings>().And(horse =>
+            {
+                horse.File.Name.ShouldBe("ntp.conf");
+                horse.Directory.Name.ShouldBe("etc");
+            });
         }
     }
 }

commit 2848f0cf36ed489685b245274820ebe3d3d11cbe

Fix DefaultValue for FileInfo and DirectoryInfo
Commit d3f4f5f208ee76a22ff39c13df4d150712685f5f introduced automatic conversion to FileInfo and DirectoryInfo but failed to properly handle the conversion if the value comes from the [DefaultValue] attribute.

Using both `var (converter, stringConstructor) = GetConverter(...)` and `var (converter, _) = GetConverter(...)` should have been a red flag!

src/Spectre.Console.Cli/Internal/Binding/CommandValueResolver.cs
@@ -78,28 +78,16 @@ internal static class CommandValueResolver
                     }
                     else
                     {
-                        var (converter, stringConstructor) = GetConverter(lookup, binder, resolver, mapped.Parameter);
-                        if (converter == null)
-                        {
-                            throw CommandRuntimeException.NoConverterFound(mapped.Parameter);
-                        }
-
                         object? value;
+                        var converter = GetConverter(lookup, binder, resolver, mapped.Parameter);
                         var mappedValue = mapped.Value ?? string.Empty;
                         try
                         {
-                            try
-                            {
-                                value = converter.ConvertFromInvariantString(mappedValue);
-                            }
-                            catch (NotSupportedException) when (stringConstructor != null)
-                            {
-                                value = stringConstructor.Invoke(new object[] { mappedValue });
-                            }
+                            value = converter.ConvertFrom(mappedValue);
                         }
                         catch (Exception exception) when (exception is not CommandRuntimeException)
                         {
-                            throw CommandRuntimeException.ConversionFailed(mapped, converter, exception);
+                            throw CommandRuntimeException.ConversionFailed(mapped, converter.TypeConverter, exception);
                         }
 
                         // Assign the value to the parameter.
@@ -130,17 +118,14 @@ internal static class CommandValueResolver
     {
         if (result != null && result.GetType() != parameter.ParameterType)
         {
-            var (converter, _) = GetConverter(lookup, binder, resolver, parameter);
-            if (converter != null)
-            {
-                result = result is Array array ? ConvertArray(array, converter) : converter.ConvertFrom(result);
-            }
+            var converter = GetConverter(lookup, binder, resolver, parameter);
+            result = result is Array array ? ConvertArray(array, converter) : converter.ConvertFrom(result);
         }
 
         return result;
     }
 
-    private static Array ConvertArray(Array sourceArray, TypeConverter converter)
+    private static Array ConvertArray(Array sourceArray, SmartConverter converter)
     {
         Array? targetArray = null;
         for (var i = 0; i < sourceArray.Length; i++)
@@ -161,14 +146,8 @@ internal static class CommandValueResolver
     }
 
     [SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "It's OK")]
-    private static (TypeConverter? Converter, ConstructorInfo? StringConstructor) GetConverter(CommandValueLookup lookup, CommandValueBinder binder, ITypeResolver resolver, CommandParameter parameter)
+    private static SmartConverter GetConverter(CommandValueLookup lookup, CommandValueBinder binder, ITypeResolver resolver, CommandParameter parameter)
     {
-        static ConstructorInfo? GetStringConstructor(Type type)
-        {
-            var constructor = type.GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, new[] { typeof(string) }, null);
-            return constructor?.GetParameters()[0].ParameterType == typeof(string) ? constructor : null;
-        }
-
         if (parameter.Converter == null)
         {
             if (parameter.ParameterType.IsArray)
@@ -180,7 +159,7 @@ internal static class CommandValueResolver
                     throw new InvalidOperationException("Could not get element type");
                 }
 
-                return (TypeDescriptor.GetConverter(elementType), GetStringConstructor(elementType));
+                return new SmartConverter(TypeDescriptor.GetConverter(elementType), elementType);
             }
 
             if (parameter.IsFlagValue())
@@ -200,13 +179,51 @@ internal static class CommandValueResolver
                 }
 
                 // Return a converter for the flag element type.
-                return (TypeDescriptor.GetConverter(value.Type), GetStringConstructor(value.Type));
+                return new SmartConverter(TypeDescriptor.GetConverter(value.Type), value.Type);
             }
 
-            return (TypeDescriptor.GetConverter(parameter.ParameterType), GetStringConstructor(parameter.ParameterType));
+            return new SmartConverter(TypeDescriptor.GetConverter(parameter.ParameterType), parameter.ParameterType);
         }
 
         var type = Type.GetType(parameter.Converter.ConverterTypeName);
-        return (resolver.Resolve(type) as TypeConverter, null);
+        if (type == null || resolver.Resolve(type) is not TypeConverter typeConverter)
+        {
+            throw CommandRuntimeException.NoConverterFound(parameter);
+        }
+
+        return new SmartConverter(typeConverter, type);
+    }
+
+    /// <summary>
+    /// Convert inputs using the given <see cref="TypeConverter"/> and fallback to finding a constructor taking a single argument of the input type.
+    /// </summary>
+    private readonly ref struct SmartConverter
+    {
+        public SmartConverter(TypeConverter typeConverter, Type type)
+        {
+            TypeConverter = typeConverter;
+            Type = type;
+        }
+
+        public TypeConverter TypeConverter { get; }
+        private Type Type { get; }
+
+        public object? ConvertFrom(object input)
+        {
+            try
+            {
+                return TypeConverter.ConvertFrom(null, CultureInfo.InvariantCulture, input);
+            }
+            catch (NotSupportedException)
+            {
+                var constructor = Type.GetConstructor(BindingFlags.Public | BindingFlags.Instance, null, new[] { input.GetType() }, null);
+                if (constructor == null)
+                {
+                    throw;
+                }
+
+                return constructor.Invoke(new[] { input });
+            }
+        }
     }
 }

test/Spectre.Console.Cli.Tests/Data/Settings/HorseSettings.cs
@@ -8,6 +8,7 @@ public class HorseSettings : MammalSettings
     public DayOfWeek Day { get; set; }
 
     [CommandOption("--file")]
+    [DefaultValue("food.txt")]
     public FileInfo File { get; set; }
 
     [CommandOption("--directory")]

test/Spectre.Console.Cli.Tests/Unit/CommandAppTests.cs
@@ -989,6 +989,7 @@ public sealed partial class CommandAppTests
         {
             horse.Legs.ShouldBe(4);
             horse.Name.ShouldBe("Arkle");
+            horse.File.Name.ShouldBe("food.txt");
         });
     }
 