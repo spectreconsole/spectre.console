a50ceabb3ee66df7978ea6fda9c017ce1ebe3055
Stuart lang <stuart.b.lang@gmail.com>
2023-09-04 15:17:19 +0100

    Remove spacebar as a selection prompt submit key

--- src/Spectre.Console/Prompts/SelectionPrompt.cs
+++ src/Spectre.Console/Prompts/SelectionPrompt.cs
@@ -103,7 +103,7 @@ public sealed class SelectionPrompt<T> : IPrompt<T>, IListPromptStrategy<T>
     /// <inheritdoc/>
     ListPromptInputResult IListPromptStrategy<T>.HandleInput(ConsoleKeyInfo key, ListPromptState<T> state)
     {
-        if (key.Key == ConsoleKey.Enter || key.Key == ConsoleKey.Spacebar || key.Key == ConsoleKey.Packet)
+        if (key.Key == ConsoleKey.Enter || key.Key == ConsoleKey.Packet)
         {
             // Selecting a non leaf in "leaf mode" is not allowed
             if (state.Current.IsGroup && Mode == SelectionMode.Leaf)
