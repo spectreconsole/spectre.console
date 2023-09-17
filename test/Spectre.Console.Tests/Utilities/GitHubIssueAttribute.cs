1009d11d4b118e41f9609359d1a3efe1d62db8cb
Patrik Svensson <patrik@patriksvensson.se>
2023-09-18 00:04:56 +0200

    Fixes TextPath rendering bugs
    * Do not emit line break when rendering
    * Don't be greedy when measuring.
    * Fix a condition that decides if the path fits in the allotted space.
    
    Closes #1307

---
+++ test/Spectre.Console.Tests/Utilities/GitHubIssueAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Spectre.Console.Tests;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public sealed class GitHubIssueAttribute : Attribute
+{
+    public string Url { get; }
+
+    public GitHubIssueAttribute(string url)
+    {
+        Url = url;
+    }
+}
