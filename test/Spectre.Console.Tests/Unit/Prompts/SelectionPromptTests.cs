60dac04b16fda0fd705ad8cc582425559a9106e0
Stuart Lang <stuart.b.lang@gmail.com>
2023-09-17 01:16:29 +0100

    Add some tests

--- test/Spectre.Console.Tests/Unit/Prompts/SelectionPromptTests.cs
+++ test/Spectre.Console.Tests/Unit/Prompts/SelectionPromptTests.cs
@@ -20,4 +20,48 @@ public sealed class SelectionPromptTests
         // Then
         console.Output.ShouldContain(@"[red]This text will never be red[/]");
     }
+
+    [Fact]
+    public void Should_Select_The_First_Leaf_Item()
+    {
+        // Given
+        var console = new TestConsole();
+        console.Profile.Capabilities.Interactive = true;
+        console.Input.PushKey(ConsoleKey.Enter);
+
+        // When
+        var prompt = new SelectionPrompt<string>()
+                .Title("Select one")
+                .Mode(SelectionMode.Leaf)
+                .AddChoiceGroup("Group one", "A", "B")
+                .AddChoiceGroup("Group two", "C", "D");
+        var selection = prompt.Show(console);
+
+        // Then
+        selection.ShouldBe("A");
+    }
+
+    [Fact]
+    public void Should_Select_The_Last_Leaf_Item_When_Wrapping_Around()
+    {
+        // Given
+        var console = new TestConsole();
+        console.Profile.Capabilities.Interactive = true;
+        console.Input.PushKey(ConsoleKey.UpArrow);
+        console.Input.PushKey(ConsoleKey.Enter);
+
+        // When
+        var prompt = new SelectionPrompt<string>()
+            .Title("Select one")
+            .Mode(SelectionMode.Leaf)
+            .WrapAround()
+            .AddChoiceGroup("Group one", "A", "B")
+            .AddChoiceGroup("Group two", "C", "D");
+        var selection = prompt.Show(console);
+
+        // Then
+        selection.ShouldBe("D");
+    }
+
+
 }
