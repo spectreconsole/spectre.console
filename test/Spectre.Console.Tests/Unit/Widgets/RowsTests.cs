bbf58ee81482e820b83bc241ef0c6c38749275db
Nils Andresen <nils@nils-andresen.de>
2023-10-02 23:30:26 +0200

    (#1188) Rows measure greedy

--- test/Spectre.Console.Tests/Unit/Widgets/RowsTests.cs
+++ test/Spectre.Console.Tests/Unit/Widgets/RowsTests.cs
@@ -5,6 +5,29 @@ namespace Spectre.Console.Tests.Unit;
 public sealed class RowsTests
 {
     [Fact]
+    [Expectation("GH-1188-Rows")]
+    [GitHubIssue("https://github.com/spectreconsole/spectre.console/issues/1188")]
+    public Task Should_Render_Rows_In_Panel_Without_Breaking_Lines()
+    {
+        // Given
+        var console = new TestConsole().Width(60);
+        var rows = new Rows(
+            new IRenderable[]
+            {
+                new Text("1"),
+                new Text("22"),
+                new Text("333"),
+            });
+        var panel = new Panel(rows);
+
+        // When
+        console.Write(panel);
+
+        // Then
+        return Verifier.Verify(console.Output);
+    }
+
+    [Fact]
     [Expectation("Render")]
     public Task Should_Render_Rows()
     {
