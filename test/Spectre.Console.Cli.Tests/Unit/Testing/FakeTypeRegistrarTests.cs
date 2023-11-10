c448d0d5f6a840822b91b2d134eab1431359d9e6
Nils Andresen <nils@nils-andresen.de>
2023-11-10 23:05:13 +0100

    (#1313) Add TypeRegistrarBaseTests for the FakeTypeRegistrar
    So it also works according to our assumptions.

---
+++ test/Spectre.Console.Cli.Tests/Unit/Testing/FakeTypeRegistrarTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Spectre.Console.Tests.Unit.Cli.Testing;
+
+public class FakeTypeRegistrarTests
+{
+    [Fact]
+    public void TheFakeTypeRegistrarPassesAllTheTestsForARegistrar()
+    {
+        ITypeRegistrar Factory() => new FakeTypeRegistrar();
+        var tester = new TypeRegistrarBaseTests(Factory);
+        tester.RunAllTests();
+    }
+}
