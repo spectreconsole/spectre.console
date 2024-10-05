015a5dd11192b673480416038ad04467d3db8a15
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-10-05 22:09:01 +0100

    Unit tests covering when the -v|--version should appear in the help output

--- src/Tests/Spectre.Console.Cli.Tests/Unit/CommandAppTests.Version.cs
+++ src/Tests/Spectre.Console.Cli.Tests/Unit/CommandAppTests.Version.cs
@@ -2,7 +2,7 @@ namespace Spectre.Console.Tests.Unit.Cli;
 
 public sealed partial class CommandAppTests
 {
-    public sealed class Version
+    public sealed partial class Version
     {
         [Fact]
         public void Should_Output_CLI_Version_To_The_Console()
