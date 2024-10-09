9919872e4a0b54d2e1244c359a344ecfe1ab437f
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-10-09 11:45:09 +0100

    Improved unit test coverage using Spectre.Console.Tests.Data.VersionCommand

--- src/Tests/Spectre.Console.Cli.Tests/Unit/CommandAppTests.Version.cs
+++ src/Tests/Spectre.Console.Cli.Tests/Unit/CommandAppTests.Version.cs
@@ -43,10 +43,6 @@ public sealed partial class CommandAppTests
         {
             // Given
             var fixture = new CommandAppTester();
-            fixture.Configure(configurator =>
-            {
-                configurator.AddCommand<EmptyCommand>("empty");
-            });
 
             // When
             var result = fixture.Run(versionOption);
@@ -54,6 +50,26 @@ public sealed partial class CommandAppTests
             // Then
             result.ExitCode.ShouldNotBe(0);
             result.Output.ShouldStartWith($"Error: Unexpected option '{versionOption.Replace("-", "")}'");
+        }
+
+        [Theory]
+        [InlineData("-v")]
+        [InlineData("--version")]
+        public void Should_Output_Application_Version_To_The_Console_With_Default_Command(string versionOption)
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.SetDefaultCommand<EmptyCommand>();
+            fixture.Configure(configurator =>
+            {
+                configurator.SetApplicationVersion("1.0");
+            });
+
+            // When
+            var result = fixture.Run(versionOption);
+
+            // Then
+            result.Output.ShouldBe("1.0");
         }
 
         [Theory]
@@ -75,26 +91,6 @@ public sealed partial class CommandAppTests
             // Then
             result.Output.ShouldBe(string.Empty);
             result.Context.ShouldHaveRemainingArgument(versionOption, new[] { (string)null });
-        }
-
-        [Theory]
-        [InlineData("-v")]
-        [InlineData("--version")]
-        public void Should_Output_Application_Version_To_The_Console_With_Default_Command(string versionOption)
-        {
-            // Given
-            var fixture = new CommandAppTester();
-            fixture.SetDefaultCommand<EmptyCommand>();
-            fixture.Configure(configurator =>
-            {
-                configurator.SetApplicationVersion("1.0");
-            });
-
-            // When
-            var result = fixture.Run(versionOption);
-
-            // Then
-            result.Output.ShouldBe("1.0");
         }
 
         [Theory]
@@ -156,16 +152,106 @@ public sealed partial class CommandAppTests
                 configurator.SetApplicationVersion("1.0");
                 configurator.AddBranch<EmptyCommandSettings>("branch", branch =>
                 {
-                    branch.AddCommand<EmptyCommand>("hello");
+                    branch.AddCommand<EmptyCommand>("empty");
                 });
             });
 
             // When
-            var result = fixture.Run("branch", "hello", versionOption);
+            var result = fixture.Run("branch", "empty", versionOption);
 
             // Then
             result.Output.ShouldBe(string.Empty);
             result.Context.ShouldHaveRemainingArgument(versionOption, new[] { (string)null });
+        }
+
+        /// <summary>
+        /// When a command with a version flag in the settings is set as the application default command,
+        /// then execute this command instead of displaying the explicitly set Application Version.
+        /// </summary>
+        [Theory]
+        [InlineData("-v")]
+        [InlineData("--version")]
+        public void Should_Execute_Default_VersionCommand_Not_Output_Application_Version_To_The_Console(string versionOption)
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.SetDefaultCommand<Spectre.Console.Tests.Data.VersionCommand>();
+            fixture.Configure(configurator =>
+            {
+                configurator.SetApplicationVersion("1.0");
+            });
+
+            // When
+            var result = fixture.Run(versionOption, "X.Y.Z");
+
+            // Then
+            result.Output.ShouldBe("VersionCommand ran, Version: X.Y.Z");
+        }
+
+        [Theory]
+        [InlineData("-v")]
+        [InlineData("--version")]
+        public void Should_Execute_VersionCommand_Not_Output_Application_Version_To_The_Console(string versionOption)
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.Configure(configurator =>
+            {
+                configurator.SetApplicationVersion("1.0");
+                configurator.AddCommand<Spectre.Console.Tests.Data.VersionCommand>("hello");
+            });
+
+            // When
+            var result = fixture.Run("hello", versionOption, "X.Y.Z");
+
+            // Then
+            result.Output.ShouldBe("VersionCommand ran, Version: X.Y.Z");
+        }
+
+        [Theory]
+        [InlineData("-v")]
+        [InlineData("--version")]
+        public void Should_Execute_Branch_Default_VersionCommand_Not_Output_Application_Version_To_The_Console(string versionOption)
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.Configure(configurator =>
+            {
+                configurator.SetApplicationVersion("1.0");
+                configurator.AddBranch<VersionSettings>("branch", branch =>
+                {
+                    branch.SetDefaultCommand<Spectre.Console.Tests.Data.VersionCommand>();
+                });
+            });
+
+            // When
+            var result = fixture.Run("branch", versionOption, "X.Y.Z");
+
+            // Then
+            result.Output.ShouldBe("VersionCommand ran, Version: X.Y.Z");
+        }
+
+        [Theory]
+        [InlineData("-v")]
+        [InlineData("--version")]
+        public void Should_Execute_Branch_VersionCommand_Not_Output_Application_Version_To_The_Console(string versionOption)
+        {
+            // Given
+            var fixture = new CommandAppTester();
+            fixture.Configure(configurator =>
+            {
+                configurator.SetApplicationVersion("1.0");
+                configurator.AddBranch<VersionSettings>("branch", branch =>
+                {
+                    branch.AddCommand<Spectre.Console.Tests.Data.VersionCommand>("hello");
+                });
+            });
+
+            // When
+            var result = fixture.Run("branch", "hello", versionOption, "X.Y.Z");
+
+            // Then
+            result.Output.ShouldBe("VersionCommand ran, Version: X.Y.Z");
         }
     }
 }
