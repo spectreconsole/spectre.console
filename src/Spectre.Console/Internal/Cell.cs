3795a90bf407613acf222d65391fcaa1b6cb3ff0
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2025-02-11 11:43:04 +0000

    Switch to Array.Fill if available

--- src/Spectre.Console/Internal/Cell.cs
+++ src/Spectre.Console/Internal/Cell.cs
@@ -13,10 +13,14 @@ internal static class Cell
 
     static Cell()
     {
+        #if !NETSTANDARD2_0
+        Array.Fill(_runeWidthCache, Sentinel);
+        #else
         for (var i = 0; i < _runeWidthCache.Length; i++)
         {
             _runeWidthCache[i] = Sentinel;
         }
+        #endif
     }
 
     public static int GetCellLength(string text)
