7aaf634594493b105c796faef149e7943589665c
Ricardo Boss <mail@ricardoboss.de>
2025-04-02 22:03:45 +0200

    Added more missing docblocks

--- src/Spectre.Console.Cli/IStartupInterceptor.cs
+++ src/Spectre.Console.Cli/IStartupInterceptor.cs
@@ -1,6 +1,13 @@
 ﻿namespace Spectre.Console.Cli;
 
+/// <summary>
+/// Represents a startup interceptor that will run before any command is executed.
+/// </summary>
 public interface IStartupInterceptor
 {
+    /// <summary>
+    /// Runs before any command is executed.
+    /// </summary>
+    /// <param name="context">The startup context.</param>
     void Intercept(StartupContext context);
 }
