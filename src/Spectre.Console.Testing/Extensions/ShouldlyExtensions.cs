c8d4f48bb7ed2b0f41e985a6d70d6c7f2aa5d915
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-07-08 15:34:44 +0100

    WIP

--- src/Spectre.Console.Testing/Extensions/ShouldlyExtensions.cs
+++ src/Spectre.Console.Testing/Extensions/ShouldlyExtensions.cs
@@ -1,7 +1,20 @@
-namespace Spectre.Console;
+namespace Spectre.Console.Testing;
 
-internal static class ShouldlyExtensions
+/// <summary>
+/// Provides extensions for testing using the Shouldly-style fluent assertions.
+/// </summary>
+public static class ShouldlyExtensions
 {
+    /// <summary>
+    /// Performs the specified action on the given object and then returns the object.
+    /// Useful for fluent testing patterns where additional assertions or operations
+    /// are chained together in a readable manner.
+    /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="item">The object to operate on.</param>
+    /// <param name="action">An action to perform on the object.</param>
+    /// <returns>The original object, to allow further chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is null.</exception>
     [DebuggerStepThrough]
     public static T And<T>(this T item, Action<T> action)
     {
