8b59ddfd41db1d34f44d0414eb8c5609a038c576
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2025-07-23 22:11:07 +0100

    Separate Spectre.Console.Cli from Spectre.Console (#1850)

--- src/Spectre.Console.Testing/Properties/Usings.cs
+++ src/Spectre.Console.Testing/Properties/Usings.cs
@@ -3,6 +3,7 @@ global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.IO;
 global using System.Linq;
+global using System.Text.RegularExpressions;
 global using System.Threading;
 global using System.Threading.Tasks;
 global using Spectre.Console.Cli;
