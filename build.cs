4a50a040f79600b640c50e09eb3c8620408a479d
Patrik Svensson <patrik@patriksvensson.se>
2025-10-26 22:32:22 +0100

    Update build script to use Cake.Sdk and .NET Make

--- build.cs
+++ build.cs
@@ -1,3 +1,5 @@
+#:sdk Cake.Sdk@5.1.25296.94-beta
+
 var target = Argument("target", "Default");
 var configuration = Argument("configuration", "Release");
 
@@ -80,7 +82,7 @@ Task("Publish-NuGet")
     .IsDependentOn("Package")
     .Does(context => 
 {
-    var apiKey = Argument<string>("nuget-key", null);
+    var apiKey = Argument<string?>("nuget-key", null);
     if(string.IsNullOrWhiteSpace(apiKey)) {
         throw new CakeException("No NuGet API key was provided.");
     }
