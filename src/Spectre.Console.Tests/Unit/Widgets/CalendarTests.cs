f04f23d4e7389291e8c2e282674df90c2cf8f287
BarrelDevelopment <aayi.ijay@gmail.com>
2025-11-26 09:16:14 +0100

    Created test cases for Calendar Widget

--- src/Spectre.Console.Tests/Unit/Widgets/CalendarTests.cs
+++ src/Spectre.Console.Tests/Unit/Widgets/CalendarTests.cs
@@ -4,6 +4,83 @@ namespace Spectre.Console.Tests.Unit;
 public sealed class CalendarTests
 {
     [Fact]
+    [Expectation("RenderNov2025woEvents")]
+    public Task Should_Render_Calendar()
+    {
+        //Given
+        var console = new TestConsole();
+        var calendar = new Calendar(2025, 11);
+
+        //When
+        console.Write(calendar);
+
+        //Then
+        return Verifier.Verify(console.Output);
+    }
+
+    [Fact]
+    [Expectation("RenderNov2025RemoveEvent")]
+    public Task Should_Render_Calendar_Remove_Event()
+    {
+        //Given
+        var console = new TestConsole();
+        var calendar = new Calendar(2025, 11)
+            .AddCalendarEvent(2025, 11, 26);
+
+        calendar.RemoveCalendarEvent(2025, 11, 26);
+        //When
+        console.Write(calendar);
+
+        //Then
+        return Verifier.Verify(console.Output);
+    }
+
+    [Fact]
+    [Expectation("RenderNov2025RemoveEvents")]
+    public Task Should_Render_Calendar_Remove_Events()
+    {
+        //Given
+        var console = new TestConsole();
+        var calendar = new Calendar(2025, 11)
+            .AddCalendarEvent(2025, 11, 1)
+            .AddCalendarEvent(2025, 11, 15)
+            .AddCalendarEvent(2025, 11, 13)
+            .AddCalendarEvent(2025, 11, 23)
+            .AddCalendarEvent(2025, 11, 27);
+
+        calendar.RemoveCalendarEvent(2025, 11, 15);
+        calendar.RemoveCalendarEvent(2025, 11, 23);
+        //When
+        console.Write(calendar);
+
+        //Then
+        return Verifier.Verify(console.Output);
+    }
+
+    [Fact]
+    [Expectation("RenderNov2025RemoveFaultyEvent")]
+    public Task Should_Render_Calendar_Remove_Faulty_Event()
+    {
+        //Given
+        var console = new TestConsole();
+        var calendar = new Calendar(2025, 11)
+            .AddCalendarEvent(2025, 11, 1)
+            .AddCalendarEvent(2025, 11, 15)
+            .AddCalendarEvent(2025, 11, 13);
+
+        calendar.RemoveCalendarEvent(2025, 11, 15);
+
+        // Remove event from May -> not in this calendar
+        calendar.RemoveCalendarEvent(2025, 5, 23);
+
+        //When
+        console.Write(calendar);
+
+        //Then
+        return Verifier.Verify(console.Output);
+    }
+
+    [Fact]
     [Expectation("Render")]
     public Task Should_Render_Calendar_Correctly()
     {
