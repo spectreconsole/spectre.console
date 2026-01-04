f9283a1419440f52f5ca548ca72a183ca3ba9479
Patrik Svensson <patrik@patriksvensson.se>
2026-01-04 23:36:54 +0100

    Move WriteAnsi and ToAnsi methods to Spectre.Console namespace

--- src/Spectre.Console/Extensions/AnsiConsoleExtensions.Ansi.cs
+++ src/Spectre.Console/Extensions/AnsiConsoleExtensions.Ansi.cs
@@ -1,18 +1,35 @@
-namespace Spectre.Console.Advanced;
+namespace Spectre.Console;
 
-/// <summary>
-/// Contains extension methods for <see cref="IAnsiConsole"/>.
-/// </summary>
-public static class AnsiConsoleExtensions
+public static partial class AnsiConsoleExtensions
 {
-    /// <param name="console">The console to write to.</param>
+    extension(AnsiConsole)
+    {
+        /// <summary>
+        /// Writes a VT/Ansi control code sequence to the console (if supported).
+        /// </summary>
+        /// <param name="sequence">The VT/Ansi control code sequence to write.</param>
+        public static void WriteAnsi(string sequence)
+        {
+            AnsiConsole.Console.WriteAnsi(sequence);
+        }
+
+        /// <summary>
+        /// Gets the VT/ANSI control code sequence for a <see cref="IRenderable"/>.
+        /// </summary>
+        /// <param name="renderable">The renderable to the VT/ANSI control code sequence for.</param>
+        /// <returns>The VT/ANSI control code sequence.</returns>
+        public static string ToAnsi(IRenderable renderable)
+        {
+            return AnsiConsole.Console.ToAnsi(renderable);
+        }
+    }
+
     extension(IAnsiConsole console)
     {
         /// <summary>
         /// Writes a VT/Ansi control code sequence to the console (if supported).
         /// </summary>
         /// <param name="sequence">The VT/Ansi control code sequence to write.</param>
-        [Obsolete("Use Spectre.Console.IAnsiConsole.WriteAnsi instead")]
         public void WriteAnsi(string sequence)
         {
             ArgumentNullException.ThrowIfNull(console);
@@ -28,7 +45,6 @@ public static class AnsiConsoleExtensions
         /// </summary>
         /// <param name="renderable">The renderable to the VT/ANSI control code sequence for.</param>
         /// <returns>The VT/ANSI control code sequence.</returns>
-        [Obsolete("Use Spectre.Console.IAnsiConsole.ToAnsi instead")]
         public string ToAnsi(IRenderable renderable)
         {
             return AnsiBuilder.Build(console, renderable);
