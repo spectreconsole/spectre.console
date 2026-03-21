86949068675df34268e44dd3fdf772576b7f9fbd
Patrik Svensson <patrik@patriksvensson.se>
2026-03-22 00:43:37 +0100

    Do explicit entry/exit. Add OSC parser

---
+++ src/Spectre.Console.Ansi/Parsing/OscCommand.cs
@@ -0,0 +1,12 @@
+namespace Spectre.Console.Ansi;
+
+public record OscCommand
+{
+    public record HyperLinkStart(string? Id, string Url) : OscCommand
+    {
+    }
+
+    public record HyperLinkEnd : OscCommand
+    {
+    }
+}
