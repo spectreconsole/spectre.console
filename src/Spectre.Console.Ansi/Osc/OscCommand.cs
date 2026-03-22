3367864b3586f18af25a15782153fdd9ba8c106a
Patrik Svensson <patrik@patriksvensson.se>
2026-03-22 13:29:08 +0100

    Remove APC. Fix some things

--- src/Spectre.Console.Ansi/Osc/OscCommand.cs
+++ src/Spectre.Console.Ansi/Osc/OscCommand.cs
@@ -1,7 +1,19 @@
 namespace Spectre.Console.Ansi;
 
+/// <summary>
+/// Represents an OSC command.
+/// </summary>
 public record OscCommand
 {
-    public record HyperLinkStart(string? Id, string Url) : OscCommand;
+    /// <summary>
+    /// Represents a hyperlink start.
+    /// </summary>
+    /// <param name="Id">The hyperlink ID.</param>
+    /// <param name="Uri">The hyperlink URI.</param>
+    public record HyperLinkStart(string? Id, string Uri) : OscCommand;
+
+    /// <summary>
+    /// Represents a hyperlink end.
+    /// </summary>
     public record HyperLinkEnd : OscCommand;
 }
