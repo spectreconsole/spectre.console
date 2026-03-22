5ec6c45b1181f4440a19fefb482b802658ea2d57
Patrik Svensson <patrik@patriksvensson.se>
2026-03-22 01:11:08 +0100

    More tests

--- src/Spectre.Console.Ansi/Parsing/Osc/OscCommand.cs
+++ src/Spectre.Console.Ansi/Parsing/Osc/OscCommand.cs
@@ -2,11 +2,6 @@ namespace Spectre.Console.Ansi;
 
 public record OscCommand
 {
-    public record HyperLinkStart(string? Id, string Url) : OscCommand
-    {
-    }
-
-    public record HyperLinkEnd : OscCommand
-    {
-    }
+    public record HyperLinkStart(string? Id, string Url) : OscCommand;
+    public record HyperLinkEnd : OscCommand;
 }
